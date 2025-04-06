36c1481fcdb7561ad138e44c37cda17169534ff1
Sachin <sachin.ec23@gmail.com>
2025-04-06 20:21:15 +0530

    query params and route params

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,23 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
+app.post("/user/:count", (req: Request, res: Response) => {
+  console.log(req.params.count);
+  console.log(req.params);
+  res.send({
+    name: "John Doe",
+    age: 30,
+  });
+});
+
+app.post("/topic", (req: Request, res: Response) => {
+  console.log(req.query);
+  res.send({
+    name: "John Doe",
+    age: 30,
+  });
+});
+
 app.use("/kitty", (req: Request, res: Response) => {
   res.send("Hello from the server");
 });
